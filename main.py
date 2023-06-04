49a9483145b90dab5e9a831d3b6b7a93a9dcd38f
FDD13 <122601072+FDD13@users.noreply.github.com>
2023-06-04 19:23:26 +0000

    Lesson_5

--- main.py
+++ main.py
@@ -1,135 +1,119 @@
-from file_operations import VERSION
-from faker import Faker
 import file_operations
-import random
 import logging
-
-
-
-
+import random
+from faker import Faker
 
 logging.basicConfig(level=logging.INFO)
 logging.info('Сообщение уровня INFO')
-
-
 fake = Faker("ru_RU")
 
-
-
-
 skills = [
-    "Стремительный прыжок", "Электрический выстрел", 
-    "Ледяной удар", "Стремительный удар", 
-    "Кислотный взгляд", "Тайный побег", 
+    "Стремительный прыжок", "Электрический выстрел",
+    "Ледяной удар", "Стремительный удар",
+    "Кислотный взгляд", "Тайный побег",
     "Ледяной выстрел", "Огненный заряд"
 ]
-runic_skills = []
-
 letters = {
-    'а': 'а͠', 
-    'б': 'б̋', 
+    'а': 'а͠',
+    'б': 'б̋',
     'в': 'в͒͠',
-    'г': 'г͒͠', 
-    'д': 'д̋', 
+    'г': 'г͒͠',
+    'д': 'д̋',
     'е': 'е͠',
-    'ё': 'ё͒͠', 
-    'ж': 'ж͒', 
+    'ё': 'ё͒͠',
+    'ж': 'ж͒',
     'з': 'з̋̋͠',
-    'и': 'и', 
-    'й': 'й͒͠', 
+    'и': 'и',
+    'й': 'й͒͠',
     'к': 'к̋̋',
-    'л': 'л̋͠', 
-    'м': 'м͒͠', 
+    'л': 'л̋͠',
+    'м': 'м͒͠',
     'н': 'н͒',
-    'о': 'о̋', 
-    'п': 'п̋͠', 
+    'о': 'о̋',
+    'п': 'п̋͠',
     'р': 'р̋͠',
-    'с': 'с͒', 
-    'т': 'т͒', 
+    'с': 'с͒',
+    'т': 'т͒',
     'у': 'у͒͠',
-    'ф': 'ф̋̋͠', 
-    'х': 'х͒͠', 
+    'ф': 'ф̋̋͠',
+    'х': 'х͒͠',
     'ц': 'ц̋',
-    'ч': 'ч̋͠', 
-    'ш': 'ш͒͠', 
+    'ч': 'ч̋͠',
+    'ш': 'ш͒͠',
     'щ': 'щ̋',
-    'ъ': 'ъ̋͠', 
-    'ы': 'ы̋͠', 
+    'ъ': 'ъ̋͠',
+    'ы': 'ы̋͠',
     'ь': 'ь̋',
-    'э': 'э͒͠͠', 
-    'ю': 'ю̋͠', 
+    'э': 'э͒͠͠',
+    'ю': 'ю̋͠',
     'я': 'я̋',
-    'А': 'А͠', 
-    'Б': 'Б̋', 
+    'А': 'А͠',
+    'Б': 'Б̋',
     'В': 'В͒͠',
-    'Г': 'Г͒͠', 
-    'Д': 'Д̋', 
+    'Г': 'Г͒͠',
+    'Д': 'Д̋',
     'Е': 'Е',
-    'Ё': 'Ё͒͠', 
-    'Ж': 'Ж͒', 
+    'Ё': 'Ё͒͠',
+    'Ж': 'Ж͒',
     'З': 'З̋̋͠',
-    'И': 'И', 
-    'Й': 'Й͒͠', 
+    'И': 'И',
+    'Й': 'Й͒͠',
     'К': 'К̋̋',
-    'Л': 'Л̋͠', 
-    'М': 'М͒͠', 
+    'Л': 'Л̋͠',
+    'М': 'М͒͠',
     'Н': 'Н͒',
-    'О': 'О̋', 
-    'П': 'П̋͠', 
+    'О': 'О̋',
+    'П': 'П̋͠',
     'Р': 'Р̋͠',
-    'С': 'С͒', 
-    'Т': 'Т͒', 
+    'С': 'С͒',
+    'Т': 'Т͒',
     'У': 'У͒͠',
-    'Ф': 'Ф̋̋͠', 
-    'Х': 'Х͒͠', 
+    'Ф': 'Ф̋̋͠',
+    'Х': 'Х͒͠',
     'Ц': 'Ц̋',
-    'Ч': 'Ч̋͠', 
-    'Ш': 'Ш͒͠', 
+    'Ч': 'Ч̋͠',
+    'Ш': 'Ш͒͠',
     'Щ': 'Щ̋',
-    'Ъ': 'Ъ̋͠', 
-    'Ы': 'Ы̋͠', 
+    'Ъ': 'Ъ̋͠',
+    'Ы': 'Ы̋͠',
     'Ь': 'Ь̋',
-    'Э': 'Э͒͠͠', 
-    'Ю': 'Ю̋͠', 
+    'Э': 'Э͒͠͠',
+    'Ю': 'Ю̋͠',
     'Я': 'Я̋',
     ' ': ' '
 }
+runic_skills = []
 
 
-
-
-def translate_skills():   
+def translate_skills():
     for skill in skills:
         for key in letters.keys():
             skill = skill.replace(key, str(letters[key]))
         runic_skills.append(skill)
-    logging.info(runic_skills)
+        logging.info(runic_skills)
 
 
 def creating_people():
-    for i in  range(10):
-        skills3 = random.sample(skills,3)
+    for i in range(10):
+        skills3 = random.sample(skills, 3)
         logging.info(skills3)
         context = {
-        "first_name": fake.first_name(),
-        "last_name": fake.last_name(),
-        "job": fake.job(),
-        "town": fake.city(),
-        "strength": random.randint(3, 18),
-        "agility": random.randint(3, 18),
-        "endurance": random.randint(3, 18),
-        "intelligence": random.randint(3, 18),
-        "luck": random.randint(3, 18),
-        "skill_1": skills3[0],
-        "skill_2": skills3[1],
-        "skill_3": skills3[2]  
-    }
+            "first_name": fake.first_name(),
+            "last_name": fake.last_name(),
+            "job": fake.job(),
+            "town": fake.city(),
+            "strength": random.randint(3, 18),
+            "agility": random.randint(3, 18),
+            "endurance": random.randint(3, 18),
+            "intelligence": random.randint(3, 18),
+            "luck": random.randint(3, 18),
+            "skill_1": skills3[0],
+            "skill_2": skills3[1],
+            "skill_3": skills3[2]
+        }
         file_operations.render_template("charsheet-0.svg", f"Files/result_{i}.svg", context)
 
 
-
-
-if __name__ ==  '__main__':
+if __name__ == '__main__':
     translate_skills()
     creating_people()
-    
